a23d2d9aab76cbe5462368bcd1d0e9bb346ae13b
Selina Lavery <selina118@hotmail.com>
2025-03-06 12:43:21 +0000

    using Math.floor and Math.random to get a random option in the getRandomComputerResult function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
+  randomOption = Math.floor(Math.random() * options.length);
+  return options[randomOption];
 }
 console.log(getRandomComputerResult());
