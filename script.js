d66ee2b41e69f2bb6f2a4aafd93ba87214c9758e
Selina Lavery <selina118@hotmail.com>
2025-03-07 21:19:40 +0000

    moved global variables to the top of script file

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
   randomOption = Math.floor(Math.random() * options.length);
@@ -18,9 +21,6 @@ function hasPlayerWonTheRound(player, computer) {
 }
 console.log(hasPlayerWonTheRound("Rock", "Scissors"));
 console.log(hasPlayerWonTheRound("Scissors", "Rock"));
-
-let playerScore = 0;
-let computerScore = 0;
 
 function getRoundResults(userOption) {
   const computerResult = getRandomComputerResult();
